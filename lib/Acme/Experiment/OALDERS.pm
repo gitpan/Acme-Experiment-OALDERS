2104bff7ad9ae67dab7d9b8f57be1052583d592a
Olaf Alders <olaf@wundersolutions.com>
2012-11-12 22:43:51 +0000

    Import of OALDERS/Acme-Experiment-OALDERS-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-Experiment-OALDERS
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         OALDERS/Acme-Experiment-OALDERS-0.01.tar.gz
    gitpan-cpan-author:       OALDERS
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/Experiment/OALDERS.pm
@@ -0,0 +1,35 @@
+use strict;
+use warnings;
+package Acme::Experiment::OALDERS;
+{
+  $Acme::Experiment::OALDERS::VERSION = '0.01';
+}
+
+1;
+
+# ABSTRACT: Experimental module to test PAUSE permissions.
+
+__END__
+=pod
+
+=head1 NAME
+
+Acme::Experiment::OALDERS - Experimental module to test PAUSE permissions.
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Olaf Alders <olaf@wundercounter.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Olaf Alders.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
